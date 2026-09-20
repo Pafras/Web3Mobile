b5531dcfdc36896d01a977af71d380d915319f8f
Pafras <pafras.prayogo@gmail.com>
2026-09-20 16:44:52 +0800

    Initial Commit

---
+++ SwiftMobile/SwiftMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMobileApp.swift
+//  SwiftMobile
+//
+//  Created by Pafras Vio Prayogo on 20/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
